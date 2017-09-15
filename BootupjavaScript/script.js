ec25c2a9ce796b1ff5bb6e7d454d105034e4e702
15497 <15497@ma-web.nl>
2017-09-15 10:16:41 +0200

    Opdracht 3
    Styleguide

--- BootupjavaScript/script.js
+++ BootupjavaScript/script.js
@@ -1,3 +1,4 @@
+//Persoon Nick hordijk
 var fristName1 = "Nick";
 var lastName1 = "Hordijk";
 var age = 26;
@@ -5,6 +6,7 @@ var hobby = "Games";
 var hooby2 = "Golf";
 var isBoy = true;
 
+//Persoon Frank muren
 var fristName2 = "Frank";
 var lastName2 = "Muren";
 var age2 = 18;
@@ -12,6 +14,7 @@ var hobby3 = "Games";
 var hooby4 = "TF";
 var isBoy = true;
 
+//Persoon Maurice de jong
 var fristName3 = "Maurice";
 var lastName3 = "de jong";
 var age3 = 18;
