0cfeb033c6cad65d14f1ea210e16840da2001484
15497 <15497@ma-web.nl>
2017-09-15 09:51:14 +0200

    opdracht 1 + 2 ???
    Ik denk dat dit goed is

---
+++ BootupjavaScript/script.js
@@ -0,0 +1,20 @@
+var fristName1 = "Nick";
+var lastName1 = "Hordijk";
+var age = 26;
+var hobby = "Games";
+var hooby2 = "Golf";
+var isBoy = true;
+
+var fristName2 = "Frank";
+var lastName2 = "Muren";
+var age2 = 18;
+var hobby3 = "Games";
+var hooby4 = "TF";
+var isBoy = true;
+
+var fristName3 = "Maurice";
+var lastName3 = "de jong";
+var age3 = 18;
+var hobby5 = "PUBG";
+var hooby6 = "minigolf";
+var isBoy = true;
